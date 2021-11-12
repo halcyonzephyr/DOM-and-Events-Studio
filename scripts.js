757a209e7dae12a63046c04c52885a5b55560296
Gus <gus.stobbs@gmail.com>
2021-11-11 20:27:27 -0600

    fixed some mcstakes addressed by mcdev and also added abort functionality

--- scripts.js
+++ scripts.js
@@ -1,32 +1,35 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener("DOMContentLoaded", (event) => {
-    let button = document.getElementById("takeoff");
+    let takeoff = document.getElementById("takeoff");
     let landButton = document.getElementById("landing");
     let missionAbort = document.getElementById("missionAbort");
     let flightStatus = document.getElementById("flightStatus");
     let shuttleBackground = document.getElementById("shuttleBackground");
     let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
 
-    button.addEventListener("click", (event) => {
-        window.confirm("Confirm that the shuttle is ready for takeoff.");
-        if (window.confirm) {
-            
-            flightStatus.innerHTML = "Shuttle in flight!";
+    takeoff.addEventListener("click", (event) => {
+        const flightConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (flightConfirmation) {
+            flightStatus.innerText = "Shuttle in flight!";
             shuttleBackground.style.backgroundColor = "blue";
-            
-            spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+            spaceShuttleHeight.innerText = parseInt(spaceShuttleHeight.innerText) + 10000;
         }
     });
 
     landButton.addEventListener("click", (event) => {
         window.alert("The shuttle is landing. Landing gear engaged.");
-        flightStatus.innerHTML = "The shuttle has landed.";
+        flightStatus.innerText = "The shuttle has landed.";
         shuttleBackground.style.backgroundColor = "green";
-        spaceShuttleHeight.innerHTML = 0;
+        spaceShuttleHeight.innerText = 0;
     });
 
     missionAbort.addEventListener("click", (event) => {
-        
+        const abortConfirmation = window.confirm("Confirm that you want to abort the mission.");
+        if (abortConfirmation) {
+            flightStatus.innerText = "Mission aborted.";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerText = 0;
+        }
     })
 });
